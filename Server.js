5df70e63e3e6bdb80f71d544491dd97b966ac039
Luka Kobalia <lkobal2013@gmail.com>
2018-06-27 10:37:46 +0400

    Final Version

--- Server.js
+++ Server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const app = express();
 
-app.use('/Public', express.static('/Public'));
-
+//app.use('/Public', express.static('/Public'));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
